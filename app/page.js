3f67e5657d851ae44222ea8c1f8d2ef45bfc7e56
seulgi <guseul9405@gmail.com>
2023-06-24 12:51:43 +0900

    db 연동 에러 수정

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-import {clientDB} from '@/utils/database.js'
+import {connectDB} from '/utils/database.js'
 
 export default async function Home() {
 
-  const client = await clientDB;
+  console.log(connectDB);
+  const client = await connectDB;
   const db = client.db('forum');
   let result = await db.collection('post').find().toArray(); //컬렉션의 모든 Document 가져오기
   console.log(result);
